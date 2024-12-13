58632b8aff92c84c22b933cc634ef6ca97690555
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-12-13 16:34:43 -0500

    Addon load changes from other branches

--- common/src/main/java/dev/creoii/luckyblock/LuckyBlockContainer.java
+++ common/src/main/java/dev/creoii/luckyblock/LuckyBlockContainer.java
@@ -75,7 +75,7 @@ public class LuckyBlockContainer {
     }
 
     public void addRandomOutcome(Identifier id, JsonObject outcome) {
-        randomOutcomes.put(id, outcome);
+        randomOutcomes.putIfAbsent(id, outcome);
     }
 
     public Map<Identifier, JsonObject> getNonrandomOutcomes() {
@@ -83,7 +83,7 @@ public class LuckyBlockContainer {
     }
 
     public void addNonRandomOutcome(Identifier id, JsonObject outcome) {
-        nonrandomOutcomes.put(id, outcome);
+        nonrandomOutcomes.putIfAbsent(id, outcome);
     }
 
     public void addItemLuckValue(Item item, int luck) {
