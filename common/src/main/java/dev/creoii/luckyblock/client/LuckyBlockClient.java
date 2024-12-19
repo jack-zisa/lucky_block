71a92916e825be0843ecd1d11e4580c9f2d7e3fb
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-12-19 00:45:15 -0500

    Begin server fixes

---
+++ common/src/main/java/dev/creoii/luckyblock/client/LuckyBlockClient.java
@@ -0,0 +1,16 @@
+package dev.creoii.luckyblock.client;
+
+import dev.creoii.luckyblock.LuckyBlockMod;
+import dev.creoii.luckyblock.util.resource.AddonAtlasSource;
+import net.minecraft.client.texture.atlas.AtlasSourceManager;
+import net.minecraft.client.texture.atlas.AtlasSourceType;
+import net.minecraft.util.Identifier;
+
+public class LuckyBlockClient {
+    public static final AtlasSourceType ADDON_ATLAS_SOURCE = new AtlasSourceType(AddonAtlasSource.CODEC);
+
+    public static void initClient() {
+        AtlasSourceManager.SOURCE_TYPE_BY_ID.put(Identifier.of(LuckyBlockMod.NAMESPACE, "addon"), ADDON_ATLAS_SOURCE);
+    }
+}
+
--- fabric/src/main/java/dev/creoii/luckyblock/fabric/client/LuckyBlockFabricClient.java
+++ fabric/src/main/java/dev/creoii/luckyblock/fabric/client/LuckyBlockFabricClient.java
@@ -1,10 +1,11 @@
 package dev.creoii.luckyblock.fabric.client;
 
+import dev.creoii.luckyblock.client.LuckyBlockClient;
 import net.fabricmc.api.ClientModInitializer;
 
 public final class LuckyBlockFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+        LuckyBlockClient.initClient();
     }
 }
